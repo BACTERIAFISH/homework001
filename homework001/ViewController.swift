645e5ee210c7756b35bef55dda31b1530d75cff4
FISH <fish@fishde-macbook-pro.local>
2018-11-02 18:06:07 +0800

    Initial Commit

---
+++ homework001/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  homework001
+//
+//  Created by FISH on 2018/11/2.
+//  Copyright © 2018年 FISH. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
